2e31f4ecd250a656c9252a8b40f3878b0409c29d
ariefrahmanpratama <rahmanarief111@gmail.com>
2023-10-24 15:09:24 +0700

    menambahkan database terupdate

--- penggajian.sql
+++ penggajian.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2023 at 03:55 AM
+-- Generation Time: Oct 24, 2023 at 10:06 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -35,6 +35,17 @@ CREATE TABLE `data_jabatan` (
   `uang_makan` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `data_jabatan`
+--
+
+INSERT INTO `data_jabatan` (`id_jabatan`, `nama_jabatan`, `gaji_pokok`, `tj_transport`, `uang_makan`) VALUES
+(2, 'Backend', '6000000', '500000', '100000'),
+(5, 'OB', '1000000', '50000', '100000'),
+(7, 'full', '1000000', '50000', '100000'),
+(9, 'Fullstack Development', '2000000', '50000', '100000'),
+(10, 'Staff Keuangan', '3000000', '100000', '50000');
+
 -- --------------------------------------------------------
 
 --
@@ -53,6 +64,16 @@ CREATE TABLE `data_kehadiran` (
   `alpha` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `data_kehadiran`
+--
+
+INSERT INTO `data_kehadiran` (`id_kehadiran`, `bulan`, `nik`, `nama_pegawai`, `jenis_kelamin`, `nama_jabatan`, `hadir`, `sakit`, `alpha`) VALUES
+(1, '102023', '1236546497', 'aerial', 'Perempuan', 'OB', 20, 1, 5),
+(2, '102023', '546456564564', 'pharact', 'Laki-laki', 'Staff Keuangan', 20, 5, 6),
+(3, '112023', '1236546497', 'aerial', 'Perempuan', 'OB', 20, 0, 10),
+(4, '112023', '546456564564', 'pharact', 'Laki-laki', 'Staff Keuangan', 20, 5, 5);
+
 -- --------------------------------------------------------
 
 --
@@ -67,7 +88,41 @@ CREATE TABLE `data_pegawai` (
   `jabatan` varchar(50) NOT NULL,
   `tanggal_masuk` date NOT NULL,
   `status` varchar(50) NOT NULL,
-  `photo` varchar(225) NOT NULL
+  `photo` varchar(225) NOT NULL,
+  `hak_akses` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `data_pegawai`
+--
+
+INSERT INTO `data_pegawai` (`id_pegawai`, `nik`, `nama_pegawai`, `jenis_kelamin`, `jabatan`, `tanggal_masuk`, `status`, `photo`, `hak_akses`) VALUES
+(1, '1234567890', 'naruto', 'Laki-laki', 'Frontend', '2023-10-18', 'Karyawan Tetap', 'default.png', 0),
+(12, '1236546497', 'aerial', 'Perempuan', 'OB', '2023-10-20', 'Pegawai Tetap', 'aerial.jpeg', 0),
+(13, '546456564564', 'pharact', 'Laki-laki', 'Staff Keuangan', '2023-10-17', 'Pegawai Tetap', 'WhatsApp_Image_2023-10-02_at_18_01_08.jpeg', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hak_akses`
+--
+
+CREATE TABLE `hak_akses` (
+  `id` int(11) NOT NULL,
+  `keterangan` varchar(50) NOT NULL,
+  `hak_akses` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `potongan_gaji`
+--
+
+CREATE TABLE `potongan_gaji` (
+  `id` int(11) NOT NULL,
+  `potongan` varchar(120) NOT NULL,
+  `jml_potongan` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -93,6 +148,12 @@ ALTER TABLE `data_pegawai`
   ADD PRIMARY KEY (`id_pegawai`);
 
 --
+-- Indexes for table `hak_akses`
+--
+ALTER TABLE `hak_akses`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -100,19 +161,25 @@ ALTER TABLE `data_pegawai`
 -- AUTO_INCREMENT for table `data_jabatan`
 --
 ALTER TABLE `data_jabatan`
-  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `data_kehadiran`
 --
 ALTER TABLE `data_kehadiran`
-  MODIFY `id_kehadiran` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_kehadiran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `data_pegawai`
 --
 ALTER TABLE `data_pegawai`
-  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `hak_akses`
+--
+ALTER TABLE `hak_akses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
