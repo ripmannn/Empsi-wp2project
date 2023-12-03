c790b0d4e2b5e700908bd60c7ca68e919f86280b
sakinatululfa <sakinatul.ulfa@gmail.com>
2023-12-04 01:04:26 +0700

    fix: table responsive in view data pegawai

--- application/views/admin/dataPegawai.php
+++ application/views/admin/dataPegawai.php
@@ -3,14 +3,18 @@
 
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800"><?= $title ?></h1>
+        <h1 class="h3 mb-0 text-gray-800">
+            <?= $title ?>
+        </h1>
     </div>
 
     <?= $this->session->flashdata('pesan') ?>
     <a class="mb-2 mt-2 btn btn-sm btn-success mb-3" href="<?= base_url('admin/dataPegawai/tambahData') ?>">
         <i class="fas fa-plus"></i> Tambah pegawai</a>
 
-    <table class="table table-bordered shadow" id="dataTable" width="100%" cellspacing="0">
+
+
+    <table class="table table-bordered shadow table-responsive" id="dataTable" width="100%" cellspacing="0">
         <thead class="thead-dark">
             <tr>
                 <th class="text-center">No</th>
@@ -27,16 +31,31 @@
         </thead>
         <tbody>
             <?php $no = 1;
-            foreach ($pegawai as $p) : ?>
+            foreach ($pegawai as $p): ?>
                 <tr>
-                    <td class="text-center"><?= $no++ ?></td>
-                    <td class="text-center"><?= $p->nik ?></td>
-                    <td class="text-center"><?= $p->nama_pegawai ?></td>
-                    <td class="text-center"><?= $p->jenis_kelamin ?></td>
-                    <td class="text-center"><?= $p->jabatan ?></td>
-                    <td class="text-center"><?= $p->tanggal_masuk ?></td>
-                    <td class="text-center"><?= $p->status ?></td>
-                    <td class="text-center"><img style="width: 100px;" src="<?= base_url('assets/photo/') . $p->photo ?>" alt=""></td>
+                    <td class="text-center">
+                        <?= $no++ ?>
+                    </td>
+                    <td class="text-center">
+                        <?= $p->nik ?>
+                    </td>
+                    <td class="text-center">
+                        <?= $p->nama_pegawai ?>
+                    </td>
+                    <td class="text-center">
+                        <?= $p->jenis_kelamin ?>
+                    </td>
+                    <td class="text-center">
+                        <?= $p->jabatan ?>
+                    </td>
+                    <td class="text-center">
+                        <?= $p->tanggal_masuk ?>
+                    </td>
+                    <td class="text-center">
+                        <?= $p->status ?>
+                    </td>
+                    <td class="text-center"><img style="width: 100px;" src="<?= base_url('assets/photo/') . $p->photo ?>"
+                            alt=""></td>
                     <?php if ($p->hak_akses == '1') { ?>
                         <td>Admin</td>
                     <?php } else { ?>
@@ -44,10 +63,12 @@
                     <?php } ?>
                     <td>
                         <center>
-                            <a class=" btn btn-sm btn-primary" href="<?= base_url('admin/dataPegawai/updateData/' . $p->id_pegawai) ?>">
+                            <a class=" btn btn-sm btn-primary"
+                                href="<?= base_url('admin/dataPegawai/updateData/' . $p->id_pegawai) ?>">
                                 <i class="fas fa-edit"></i>
                             </a>
-                            <a class=" btn btn-sm btn-danger" onclick="return confirm('YAKIN DELETE DATA INI ?')"  href="<?= base_url('admin/dataPegawai/deleteData/' . $p->id_pegawai) ?>" >
+                            <a class=" btn btn-sm btn-danger" onclick="return confirm('YAKIN DELETE DATA INI ?')"
+                                href="<?= base_url('admin/dataPegawai/deleteData/' . $p->id_pegawai) ?>">
                                 <i class="fas fa-trash"></i>
                             </a>
                         </center>
